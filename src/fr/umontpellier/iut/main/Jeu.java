58e9a441cd7a7ee84902b103b679595cff90f37f
johan-daniel <johan.daniel@etu.umontpellier.fr>
2020-11-14 17:54:29 +0100

    Début interface

--- src/fr/umontpellier/iut/main/Jeu.java
+++ src/fr/umontpellier/iut/main/Jeu.java
@@ -16,14 +16,17 @@ public class Jeu extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("echiquier.fxml"));
+        primaryStage.setTitle("Stale Chess");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
+        launch(args);
+
         ModelEchiquier e = new ModelEchiquier();
         e.setRoiTour();
         boolean verif = false;
--- src/fr/umontpellier/iut/main/controller/ControllerEchequier.java
+++ src/fr/umontpellier/iut/main/controller/ControllerEchequier.java
@@ -1,4 +1,32 @@
 package fr.umontpellier.iut.main.controller;
 
-public class ControllerEchequier {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerEchequier implements Initializable {
+
+    @FXML
+    private GridPane fenetre;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("ControllerEchiquier");
+        fenetre.setGridLinesVisible(true);
+
+        for(int i=0; i<8; i++) {
+            for(int j=0; j<8; j++) {
+                Color color = (i+j) % 2 == 0 ? Color.WHITE : Color.BLACK;
+                Rectangle rct = new Rectangle(500/8., 500/8., color);
+                GridPane.setConstraints(rct, i, j);
+                fenetre.getChildren().add(rct);
+            }
+        }
+    }
 }

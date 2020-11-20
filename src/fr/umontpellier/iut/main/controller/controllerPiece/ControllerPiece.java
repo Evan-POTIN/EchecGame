1ea68e7694de264373a6235ecee6edad22f3d5a1
johan-daniel <johan.daniel@etu.umontpellier.fr>
2020-11-20 17:48:56 +0100

    classes rajoutés

---
+++ src/fr/umontpellier/iut/main/controller/controllerPiece/ControllerPiece.java
@@ -0,0 +1,30 @@
+package fr.umontpellier.iut.main.controller.controllerPiece;
+
+import fr.umontpellier.iut.main.model.ModelPiece;
+import fr.umontpellier.iut.main.model.ModelRoi;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerPiece implements Initializable {
+    @FXML
+    private Text icon;
+    private ModelPiece piece;
+
+    public ControllerPiece(ModelPiece piece) {
+        this.piece = piece;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        if(piece != null) {
+            icon.setText(piece.getUnicode());
+        }
+        else {
+            icon.setText("TEST");
+        }
+    }
+}

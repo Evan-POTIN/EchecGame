824612fcdd4d1b9c46fb9a307326e063766080ed
johan-daniel <johan.daniel@etu.umontpellier.fr>
2020-12-12 18:17:57 +0100

    Création classes

--- src/fr/umontpellier/iut/main/joueur/Arbre.java
+++ src/fr/umontpellier/iut/main/joueur/Arbre.java
@@ -1,19 +1,35 @@
 package fr.umontpellier.iut.main.joueur;
 
+import fr.umontpellier.iut.main.model.ModelEchiquier;
+
 import java.util.ArrayList;
 
 public class Arbre {
 
+    private ModelEchiquier coup;
     private int valeurCoup;
     private ArrayList<Arbre> fils;
 
-    public Arbre() {}
+    public Arbre(ModelEchiquier me, int vc) { coup = me; valeurCoup = vc; }
 
-    public int getValeurCoup() {
-        return valeurCoup;
+    public ModelEchiquier getCoup() {
+        return coup;
     }
 
     public ArrayList<Arbre> getFils() {
         return fils;
+    }
+
+    public void addFils(ModelEchiquier me, int vc) {
+        if(fils.isEmpty()) {
+            fils.add(new Arbre(me,vc));
+        }
+        else {
+            int i=0;
+            while(vc <= fils.get(i).valeurCoup || i < fils.size()) {
+                i++;
+            }
+            fils.add(new Arbre(me,vc));
+        }
     }
 }

2d4e436110dfae0604517a0c301d4056941ce1fb
johan-daniel <johan.daniel@etu.umontpellier.fr>
2020-12-14 10:31:43 +0100

    1ère fonction d'eval

--- src/fr/umontpellier/iut/main/joueur/Minmax.java
+++ src/fr/umontpellier/iut/main/joueur/Minmax.java
@@ -19,11 +19,17 @@ public class Minmax extends Joueur{
 
         for(int i=0; i<8; i++) {
             for(int j=0; j<8; j++) {
-                eval += (me.getCase(i,j).getPiece().getClr() == Couleurs.BLANC) ? me.getCase(i,j).getPiece().getValeur() : -me.getCase(i,j).getPiece().getValeur();
+
+                // Si la pièce est blanche on ajoute sa valeur sinon on soustrait
+                eval += (!me.getCase(i,j).estVide() && me.getCase(i,j).getPiece().getClr() == Couleurs.BLANC)
+                        ? me.getCase(i,j).getPiece().getValeur()
+                        : -me.getCase(i,j).getPiece().getValeur();
             }
         }
 
-        eval -= Math.abs(me.getRoiBlanc().getPosition()[0] - me.getRoiNoir().getPosition()[0]) + Math.abs(me.getRoiBlanc().getPosition()[1] - me.getRoiNoir().getPosition()[1]);
+        // On soustrait la distance entre les 2 rois
+        eval -= Math.abs(me.getRoiBlanc().getPosition()[0] - me.getRoiNoir().getPosition()[0])
+                + Math.abs(me.getRoiBlanc().getPosition()[1] - me.getRoiNoir().getPosition()[1]);
 
         return eval;
     }
